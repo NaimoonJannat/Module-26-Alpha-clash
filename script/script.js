b163617fe6a60da5e41d4d62b75be0c9b1986248
NaimoonJannat <naimoonjannat@gmail.com>
2024-02-16 23:49:52 +0600

    Keyboard random letter generate

--- script/script.js
+++ script/script.js
@@ -1,10 +1,18 @@
 function play(){
-    // const homeSection = document.getElementById('home');
-    // homeSection.classList.add('hidden');
-
-    // const playSection = document.getElementById('playground');
-    // playSection.classList.remove('hidden');
-
+    // by clicking the play button on home 
     hideElement('home');
     showElement('playground');
+    continuePlaying();
+    
+}
+function continuePlaying(){
+    // generate a random letter
+    const alphabet= getRandomLetter();
+
+    // show it on the screen 
+    const currentLetter= document.getElementById('current-letter');
+    currentLetter.innerText=alphabet;
+
+    // set bg color of keyboard 
+    setBgColor(alphabet);
 }
--- script/utility.js
+++ script/utility.js
@@ -7,3 +7,18 @@ function showElement(elementId){
     const playSection = document.getElementById(elementId);
     playSection.classList.remove('hidden');
 }
+
+function getRandomLetter(){
+    const alphaString='abcdefghijklmnopqrstuvwxyz';
+    const alphabets= alphaString.split('');
+
+    const randomNum=Math.random()*25;
+    const index= Math.round(randomNum);
+    const alphabet= alphabets[index];
+    return alphabet;
+}
+
+function setBgColor(elementId){
+    const element= document.getElementById(elementId);
+    element.classList.add('bg-orange-400');
+}
