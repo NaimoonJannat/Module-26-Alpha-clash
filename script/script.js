e66a010a6170418985c0f91c34c0037cfd70aa1f
NaimoonJannat <naimoonjannat@gmail.com>
2024-02-17 12:32:50 +0600

    Updated

--- script/script.js
+++ script/script.js
@@ -3,8 +3,60 @@ function play(){
     hideElement('home');
     showElement('playground');
     continuePlaying();
-    
 }
+
+function handleKeyboardKeyup(event){
+    const playerPressed= event.key;
+    console.log('player Pressed', playerPressed);
+
+    // Get expected to press 
+    const currentAlphabetElement = document.getElementById('current-letter');
+    const currentAlphabet=currentAlphabetElement.innerText;
+    const expectedAlphabet= currentAlphabet.toLowerCase();
+    console.log(playerPressed, expectedAlphabet);
+
+    // Check matched or not 
+
+    if(playerPressed === expectedAlphabet){
+        console.log('You get a point');
+        // update score 
+        // 1) get the Score 
+        const getScoreById = document.getElementById('current-score');
+        const currentScoreText = getScoreById.innerText;
+        const currentScore= parseInt(currentScoreText);
+        // 2) increase it 
+        const newScore= currentScore+1;
+    
+        // 3) Display the score 
+        getScoreById.innerText= newScore;
+        // start a new round 
+        removeBgColor(expectedAlphabet);
+        continuePlaying();
+    }
+    else{
+        console.log('You have lost one life!');
+        // Decrease life 
+        // 1) get currentLife 
+        const getLifeById = document.getElementById('current-life');
+        const currentLifeText = getLifeById.innerText;
+        const currentLife= parseInt(currentLifeText);
+
+        // 2) decrease it 
+        const newLife= currentLife - 1;
+
+        // 3)Display the current life 
+        getLifeById.innerText= newLife;
+
+        if (newLife==0){
+            hideElement('playground');
+            showElement('scoreBoard');
+        }
+    }
+}
+
+document.addEventListener('keyup',handleKeyboardKeyup);
+
+
 function continuePlaying(){
     // generate a random letter
     const alphabet= getRandomLetter();
@@ -15,4 +67,5 @@ function continuePlaying(){
 
     // set bg color of keyboard 
     setBgColor(alphabet);
+    
 }
--- script/utility.js
+++ script/utility.js
@@ -22,3 +22,8 @@ function setBgColor(elementId){
     const element= document.getElementById(elementId);
     element.classList.add('bg-orange-400');
 }
+
+function removeBgColor(elementId){
+    const element= document.getElementById(elementId);
+    element.classList.remove('bg-orange-400');
+}
